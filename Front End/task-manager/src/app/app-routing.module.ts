d444750095161692397eed2e62d9944e6b02d720
Nick King <nickking93@live.com>
2024-08-08 16:01:41 -0500

    Adds privacy policy component and content

--- Front End/task-manager/src/app/app-routing.module.ts
+++ Front End/task-manager/src/app/app-routing.module.ts
@@ -4,14 +4,13 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { TaskListComponent } from './task-list/task-list.component';
 import { TaskDetailsComponent } from './task-details/task-details.component';
-// import { AuthGuard } from './auth/auth.guard'; // AuthGuard for protecting routes
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component'; // Import the new component
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  // { path: 'tasks', component: TaskListComponent, canActivate: [AuthGuard] },
-  // { path: 'tasks/:id', component: TaskDetailsComponent, canActivate: [AuthGuard] },
+  { path: 'privacy-policy', component: PrivacyPolicyComponent }, // Add the new route
   { path: '**', redirectTo: '/login' } // Wildcard route for a 404 page
 ];
 
