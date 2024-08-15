23d6adc8282d7490b7cec1ad316c5188a3ae1b8e
Nick King <nickking93@live.com>
2024-08-15 13:16:14 -0500

    Update task.model.spec.ts to properly instanciate test task

--- frontend/task-manager/src/app/models/task.model.spec.ts
+++ frontend/task-manager/src/app/models/task.model.spec.ts
@@ -2,6 +2,13 @@ import { Task } from './task.model';
 
 describe('Task', () => {
   it('should create an instance', () => {
-    expect(new Task()).toBeTruthy();
+    const task = new Task(
+      'Sample Task',           
+      'This is a sample task', 
+      '2024-08-15',            
+      'High',                  
+      false                    
+    );
+    expect(task).toBeTruthy();
   });
 });
