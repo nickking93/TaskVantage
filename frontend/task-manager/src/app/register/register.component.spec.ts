e2c3d718b6611e73a93e3a41ba12f957286673b0
Nick King <nickking93@live.com>
2024-08-15 13:55:50 -0500

    Fix unit test for register component

--- frontend/task-manager/src/app/register/register.component.spec.ts
+++ frontend/task-manager/src/app/register/register.component.spec.ts
@@ -1,7 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';  // Import BrowserAnimationsModule
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';  // Import MatFormFieldModule
+import { MatInputModule } from '@angular/material/input';            // Import MatInputModule
+import { MatIconModule } from '@angular/material/icon';              // Import MatIconModule
+import { MatButtonModule } from '@angular/material/button';          // Import MatButtonModule
 import { RegisterComponent } from './register.component';
 
 describe('RegisterComponent', () => {
@@ -13,9 +17,13 @@ describe('RegisterComponent', () => {
       imports: [
         HttpClientTestingModule,
         RouterTestingModule,
-        BrowserAnimationsModule  // Add BrowserAnimationsModule here
+        BrowserAnimationsModule,
+        MatFormFieldModule,  // Add Angular Material modules here
+        MatInputModule,
+        MatIconModule,
+        MatButtonModule
       ],
-      declarations: [RegisterComponent]  // Declare RegisterComponent
+      declarations: [RegisterComponent]
     })
     .compileComponents();
 
