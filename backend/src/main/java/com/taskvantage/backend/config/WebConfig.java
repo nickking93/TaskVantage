305efd739c078da040de491dda68be58b6158e6a
Nick King <nickking93@live.com>
2024-08-17 12:57:57 -0500

    Update backend to accept requests from production front end URL

--- backend/src/main/java/com/taskvantage/backend/config/WebConfig.java
+++ backend/src/main/java/com/taskvantage/backend/config/WebConfig.java
@@ -14,7 +14,11 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("https://tightly-dear-boxer.ngrok-free.app", "http://localhost:4200")
+                        .allowedOrigins(
+                                "https://tightly-dear-boxer.ngrok-free.app",
+                                "http://localhost:4200",
+                                "https://taskvantage-frontend-cbaab3e2bxcpbyb8.eastus-01.azurewebsites.net"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
