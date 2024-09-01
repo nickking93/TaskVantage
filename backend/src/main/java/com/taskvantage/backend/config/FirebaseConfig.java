ecb049aad7dfab5ef07e25e306a2b70c9dc4ed80
Nick King <nickking93@live.com>
2024-09-01 06:02:42 -0500

    Update Firebase config

--- backend/src/main/java/com/taskvantage/backend/config/FirebaseConfig.java
+++ backend/src/main/java/com/taskvantage/backend/config/FirebaseConfig.java
@@ -3,12 +3,13 @@ package com.taskvantage.backend.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
-import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,8 +17,10 @@ import java.io.InputStream;
 @Configuration
 public class FirebaseConfig {
 
-    @Value("${FIREBASE_CONFIG:}")
-    private String firebaseConfig;
+    private static final Logger logger = LoggerFactory.getLogger(FirebaseConfig.class);
+
+    @Value("${FIREBASE_CONFIG_PATH:}")
+    private String firebaseConfigPath;
 
     private final Environment env;
 
@@ -29,18 +32,16 @@ public class FirebaseConfig {
     public FirebaseApp initializeFirebase() throws IOException {
         FirebaseOptions options;
 
-        if (env.acceptsProfiles("prod")) {
-            // Production environment: FIREBASE_CONFIG contains JSON content
-            InputStream serviceAccount = new ByteArrayInputStream(firebaseConfig.getBytes());
+        logger.info("Active profiles: {}", (Object) env.getActiveProfiles());
+
+        if (env.acceptsProfiles("prod") || env.acceptsProfiles("dev")) {
+            logger.info("Using Firebase configuration from file: {}", firebaseConfigPath);
+            InputStream serviceAccount = new FileInputStream(firebaseConfigPath);
             options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
         } else {
-            // Development environment: FIREBASE_CONFIG contains the path to the JSON file
-            InputStream serviceAccount = new FileInputStream(firebaseConfig);
-            options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
+            throw new IllegalStateException("No valid environment profile found!");
         }
 
         return FirebaseApp.initializeApp(options);
